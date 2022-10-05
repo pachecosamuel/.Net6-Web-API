537cdefd40584e9585ed55c0254547189c09a273
Samuel Pacheco <pacheco.samuel@hotmail.com>
2022-10-05 08:48:57 -0300

    First day of study

--- Course/Class-14-FirstAPI/Program.cs
+++ Course/Class-14-FirstAPI/Program.cs
@@ -1,8 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+//1°
 app.MapGet("/", () => "This is my first API in .Net platform!\nI'm so proud of me.\nThird Line!");
+
+//2°
 app.MapPost("/", () => new { Name = "Samuel Pacheco", Age = 24, Salary = 19562.20 });
+
+//3°
 app.MapGet("/Header", (HttpResponse response) =>
 {
     response.Headers.Add("MyHeader", "Samuel Millionare");
