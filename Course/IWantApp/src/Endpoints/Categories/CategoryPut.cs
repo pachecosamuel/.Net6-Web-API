ad361d9092637b74c491c7638bac79530563cac6
pachecosamuel <pacheco.samuel@hotmail.com>
2022-10-21 11:36:07 -0300

    Crud created

---
+++ Course/IWantApp/src/Endpoints/Categories/CategoryPut.cs
@@ -0,0 +1,25 @@
+﻿using IWantApp.Domain.Products;
+using IWantApp.Infra.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IWantApp.Endpoints.Categories;
+
+public class CategoryPut
+{
+    public static string Template => "/categories/{id}";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action([FromRoute] Guid id,CategoryRequest categoryRequest, ApplicationDbContext dbContext)
+    {
+        var category = dbContext.Categories.Where(c => c.Id == id).FirstOrDefault();
+
+        category.Name = categoryRequest.Name;
+        category.Active = categoryRequest.Active;
+
+        dbContext.SaveChanges();
+
+        return Results.Ok();
+    }
+
+}
